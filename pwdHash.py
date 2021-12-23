91dbd13f84259d1aa6609a707a993d87b6daa39a
Martin Mouly <martin@mouly.name>
2021-12-23 10:07:59 +0100

    Add files via upload

--- pwdHash.py
+++ pwdHash.py
@@ -7,14 +7,22 @@ def hash_password(pwd):
 
 def encryption_machine(msg):
   # encrypt using AES-SIV
+  ciphertext=""
   return ciphertext
 
 def save_to_database(user, pwd):
   # use a file as a database
   # format: user, hashed_password
   # for example: file.write(user, hash_password(pwd))
+  file = open(database, "a")
+  encrypted_pwd=pwd
+  file.write(f"{user}, {encrypted_pwd}\n")
+  file.close()
 
 def check_password(user, pwd):
   # read from database
   # and check for authentication
-  return false/true
+  file = open(database, "r")
+  for line in file.readlines():
+      if user
+  return True
